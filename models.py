a26307c713a9f775cfd36f1bbe2de4842b2182b2
xieyi <breadbread1984@163.com>
2020-06-14 17:59:44 +0800

    update models

--- models.py
+++ models.py
@@ -3,12 +3,14 @@
 import numpy as np;
 import tensorflow as tf;
 
-def NonLocalBlock(hidden_dim = 1024):
+def NonLocalBlock(hidden_dim = 1024, pool_size = (2,2,2)):
 
   inputs = tf.keras.Input((None, None, None, hidden_dim)); # inputs.shape = (batch, seq_length, height, width, hidden_dim)
   query = tf.keras.layers.Conv3D(filters = 512, kernel_size = (1,1,1), padding = 'same')(inputs); # query.shape = (batch, seq_length, height, width, 512)
   key = tf.keras.layers.Conv3D(filters = 512, kernel_size = (1,1,1), padding = 'same')(inputs); # key.shape = (batch, seq_length, height, width, 512)
+  key = tf.keras.layers.MaxPool3D(pool_size = pool_size)(key); # key.shape = (batch, seq_length, height, width, 512)
   value = tf.keras.layers.Conv3D(filters = 512, kernel_size = (1,1,1), padding = 'same')(inputs); # value.shape = (batch, seq_length, height, width, 512)
+  value = tf.keras.layers.MaxPool3D(pool_size = pool_size)(value); # value.shape = (batch, seq_length, height, width, 512)
   logits = tf.keras.layers.Lambda(lambda x: tf.linalg.matmul(tf.keras.layers.Reshape((-1, 512))(x[0]), tf.keras.layers.Reshape((-1, 512))(x[1]), 
                                                              transpose_b = True))([query, key]); # logits.shape = (batch, seq_length * height * width, seq_length * height * width)
   attention = tf.keras.layers.Softmax(axis = -1)(logits); # attention.shape = (batch, seq_length * height * width, seq_length * height * width)
@@ -22,6 +24,6 @@ if __name__ == "__main__":
 
   assert tf.executing_eagerly();
   nonlocalblock = NonLocalBlock();
-  a = tf.constant(np.random.normal(size = (8,10,4,3, 1024)));
+  a = tf.constant(np.random.normal(size = (8,10,8,4, 1024)));
   b = nonlocalblock(a);
   nonlocalblock.save('nonlocalblock.h5');
